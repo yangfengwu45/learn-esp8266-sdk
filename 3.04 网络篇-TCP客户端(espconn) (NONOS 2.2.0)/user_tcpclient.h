c0793dcea0577d1e6929026feea18a43497792e3
yagnfengwu45 <946029359@qq.com>
2021-08-14 09:07:16 +0800

    Signed-off-by: yagnfengwu45 <946029359@qq.com>

---
+++ 3.04 网络篇-TCP客户端(espconn) (NONOS 2.2.0)/user_tcpclient.h
@@ -0,0 +1,19 @@
+#ifndef __USER_TCPCLIENT_H__
+#define __USER_TCPCLIENT_H__
+#include "ip_addr.h"
+#include "espconn.h"
+
+
+void user_tcp_recv_cb(void *arg, char *pusrdata, unsigned short length);
+void user_tcp_sent_cb(void *arg);
+void user_tcp_discon_cb(void *arg);
+void user_tcp_send_data(uint8 *psent, uint16 length);
+void user_tcp_connect_cb(void *arg);
+void user_tcp_recon_cb(void *arg, sint8 err);
+void user_dns_found(const char *name, ip_addr_t *ipaddr, void *arg);
+void user_dns_check_cb(void *arg);
+void user_tcp_connect();
+void user_tcp_init(char *user_tcp_server_ip,int user_remote_port);
+uint8 UTILS_StrToIP(const char* str, void *ip);
+#endif
+
